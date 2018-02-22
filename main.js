eee46f8783c48ce8f0906db9cc36f9942ea576ef
Cody Otterbine <dauntless.mail@gmail.com>
2018-02-21 22:40:34 -0600

    Reducing Memory Usage

--- main.js
+++ main.js
@@ -40,12 +40,20 @@ document.addEventListener('DOMContentLoaded', function() {
         emitEvent('scene-started', ['#image-2']);
     });
 
+    document.querySelector('#cylinder-8').addEventListener('click', function (e) {
+        emitEvent('cylinder-8-clicked', ['#obj-model-4', '#obj-model-4', '#obj-model-4', '#obj-model-3', '#obj-model-3', '#obj-model-3']);
+    });
+
+    document.querySelector('#box-79').addEventListener('click', function (e) {
+        emitEvent('box-79-clicked', ['#image-3']);
+    });
+
     document.querySelector('#box-76').addEventListener('click', function (e) {
-        emitEvent('box-76-clicked', ['#point-light-4', '#point-light']);
+        emitEvent('box-76-clicked', ['#point-light-3', '#point-light-2', '#point-light-4', '#point-light']);
     });
 
     document.querySelector('#box-77').addEventListener('click', function (e) {
-        emitEvent('box-77-clicked', ['#point-light-4', '#point-light']);
+        emitEvent('box-77-clicked', ['#point-light-2', '#point-light-4', '#point-light']);
     });
 
     document.querySelector('#box-78').addEventListener('click', function (e) {
